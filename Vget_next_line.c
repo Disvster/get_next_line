b55e1dfb71e50cc730209e36945e1cefec2343f1
Manuel Maria Carreiro Couto <manmaria@c1r10s5.42porto.com>
2025-05-23 20:41:37 +0100

    stated working on new gnl

---
+++ Vget_next_line.c
@@ -0,0 +1,174 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: manmaria <manmaria@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/15 22:30:39 by manmaria          #+#    #+#             */
+/*   Updated: 2025/05/15 23:10:26 by manmaria         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Vget_next_line.h"
+
+int	find_line(char *s)
+{
+	if (!s)
+		return (1);
+	while (*s)
+	{
+		if (*s == '\n')
+			return (0);
+		s++;
+	}
+	return (1);
+}
+
+char	*ft_trimstash(char *stash)
+{
+	char	*line;
+	size_t	len;
+	size_t	i;
+
+	if (!stash)
+		return (NULL);
+	len = ft_strclen(stash, '\n') + 1;
+	line = ft_calloc(sizeof(char), len + 1);
+	if (!line)
+		return (free(NULL);
+	i = 0;
+	while (i < len)
+	{
+		line[i] = stash[i];
+		i++;
+	}
+	return (line);
+}
+
+char	*ft_stash_copycat(char *stash, char *buffer)
+{
+	char	*tmp;
+	size_t	ttlen;
+
+	if (stash)
+	{
+		tmp = ft_strdup(stash);
+		free(stash);
+		stash = NULL;
+	}
+	else
+	{
+		tmp = ft_calloc(sizeof(char), 1);
+		stash = NULL;
+	}
+	if (!tmp)
+		return (NULL);
+	stash = ft_strjoin(tmp, buffer);
+	free(buffer);
+	buffer = NULL;
+	free(tmp);
+	if (!stash)
+		return (NULL);
+	return (stash);
+}
+
+void	ft_free_stash(char **stash, char *line)
+{
+	char	*tmp;
+	size_t	flen;
+
+	if (!line)
+	{
+		free (*stash);
+		*stash = NULL;
+		return ;
+	}
+	flen = ft_strclen(*stash, 0) - ft_strclen(line, 0) + 1;
+	tmp = ft_strdup(*stash);
+	if (!tmp)
+	{
+		free(*stash);
+		*stash = NULL;
+		free(line);
+		line = NULL;
+		return ;
+	}
+	free(*stash);
+	*stash = ft_calloc(sizeof(char), flen);
+	if (!*stash)
+	{
+		free(tmp);
+		free(line);
+		line = NULL;
+		return ;
+	}
+	ft_strlcpy(*stash, tmp + ft_strclen(line, 0), flen);
+	free(tmp);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*stash;
+	char		*buffer;
+	char		*line;
+	ssize_t		bread;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	bread = 1;
+	line = NULL;
+	buffer = NULL;
+	while (bread > 0 && find_line(stash))
+	{
+		buffer = ft_calloc(sizeof(char), BUFFER_SIZE + 1);
+		if (!buffer)
+			return (NULL);
+		bread = read(fd, buffer, BUFFER_SIZE);
+		if (bread == 0)
+			break ;
+		if (bread == -1)
+			return (ft_free_stash(&stash, NULL), free(buffer), NULL);
+		stash = ft_stash_copycat(stash, buffer);
+		if (!stash)
+			return (NULL);
+	}
+	line = ft_trimstash(stash);
+	ft_free_stash(&stash, line);
+	return (free(buffer), line);
+}
+
+// #include <stdio.h>
+// int	main(int ac, char **av)
+// {
+// 	(void)ac;
+// 	char	*line = NULL;
+// 	int	fd = open(av[1], O_RDONLY);
+// 	printf("%s", line = get_next_line(fd));
+// 	free(line);
+// 	printf("%s", line = get_next_line(fd));
+// 	free(line);
+// 	printf("%s", line = get_next_line(fd));
+// 	free(line);
+// 	printf("%s", line = get_next_line(fd));
+// 	free(line);
+// 	printf("%s", line = get_next_line(fd));
+// 	free(line);
+// 	printf("%s", line = get_next_line(fd));
+// 	printf("%s", line = get_next_line(fd));
+// 	printf("%s", line = get_next_line(fd));
+// }
+//
+// #include <stdio.h>
+// int	main(int ac, char **av)
+// {
+// 	(void)ac;
+// 	char	*line = NULL;
+// 	int	fd = open(av[1], O_RDONLY);
+// 	while ((line = get_next_line(fd)))
+// 	{
+// 		printf("%s", line);
+// 		free(line);
+// 	}
+// 	close(fd);
+// }
---
+++ Vget_next_line_utils.c
@@ -0,0 +1,131 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: manmaria <manmaria@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/15 23:11:26 by manmaria          #+#    #+#             */
+/*   Updated: 2025/05/15 23:11:54 by manmaria         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Vget_next_line.h"
+
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	i;
+	size_t	ltotal;
+	char	*scat;
+
+	if (!s1)
+		return (0);
+	ltotal = ft_strclen(s1, 0) + ft_strclen(s2, 0);
+	scat = (char *)malloc(sizeof(char) * (ltotal + 1));
+	if (!scat)
+		return (NULL);
+	i = 0;
+	while (i < ft_strclen(s1, 0))
+	{
+		scat[i] = s1[i];
+		i++;
+	}
+	while (i < ltotal)
+	{
+		scat[i] = s2[i - ft_strclen(s1, 0)];
+		i++;
+	}
+	scat[i] = '\0';
+	return (scat);
+}
+
+size_t	ft_strclen(const char *s, int c)
+{
+	size_t	i;
+
+	if (!s)
+		return (0);
+	i = 0;
+	while (s[i] && s[i] != (char)c)
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(const char *s)
+{
+	char	*dup;
+	size_t	i;	
+	size_t	len;
+
+	if (!s)
+		return (NULL);
+	i = 0;
+	len = ft_strclen(s, 0);
+	dup = (char *)malloc(sizeof(char) * (len + 1));
+	if (!dup)
+		return (NULL);
+	while (s[i])
+	{
+		dup[i] = s[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
+
+char	*ft_calloc(size_t nmemb, size_t size)
+{
+	char	*arr;
+	size_t	mult;
+	size_t	i;
+
+	if (nmemb != 0 && size != 0 && (nmemb > ((size_t)-1 / size)))
+		return (NULL);
+	mult = nmemb * size;
+	arr = malloc(nmemb * size);
+	if (!arr)
+		return (NULL);
+	i = 0;
+	while (i < mult)
+		arr[i++] = '\0';
+	return (arr);
+}
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	src_len;
+	size_t	dst_len;
+	size_t	i;
+
+	if (!dst && !size)
+		return (0);
+	dst_len = ft_strclen(dst, 0);
+	src_len = ft_strclen(src, 0);
+	if (size == 0 || dst_len >= size)
+		return (src_len + size);
+	i = 0;
+	while (src[i] && (i + dst_len + 1) < size)
+	{
+		dst[dst_len + i] = src[i];
+		i++;
+	}
+	dst[dst_len + i] = '\0';
+	return (src_len + dst_len);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	if (!size)
+		return (ft_strclen(src, 0));
+	while (src[i] && i + 1 < size)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (ft_strclen(src, 0));
+}
